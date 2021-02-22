b3601d00df5a16241db22b6fa5c5de155b3d51d4
DESKTOP-20POM8C\Pablo <pablo-pinheiro@hotmail.com>
2021-02-22 15:04:03 -0300

    configurado nova tela para deslogar, utilizando o mesmo conceito da lista

--- app/src/main/java/br/com/alura/aluraesporte/ui/fragment/DetalhesProdutoFragment.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/fragment/DetalhesProdutoFragment.kt
@@ -1,9 +1,7 @@
 package br.com.alura.aluraesporte.ui.fragment
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.NavArgs
@@ -13,6 +11,7 @@ import br.com.alura.aluraesporte.R
 import br.com.alura.aluraesporte.extensions.formatParaMoedaBrasileira
 import br.com.alura.aluraesporte.ui.activity.CHAVE_PRODUTO_ID
 import br.com.alura.aluraesporte.ui.viewmodel.DetalhesProdutoViewModel
+import br.com.alura.aluraesporte.ui.viewmodel.LoginViewModel
 import kotlinx.android.synthetic.main.detalhes_produto.*
 import org.koin.android.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
@@ -29,6 +28,14 @@ class DetalhesProdutoFragment : Fragment() {
     private val controlador by lazy{
         findNavController()
     }
+
+    private val loginViewModel: LoginViewModel by viewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -70,6 +77,24 @@ class DetalhesProdutoFragment : Fragment() {
                 detalhes_produto_preco.text = produto.preco.formatParaMoedaBrasileira()
             }
         })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.menu_lista_produtos, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item?.itemId == R.id.menu_lista_produtos_deslogar) {
+            loginViewModel.desloga()
+            vaiParaLogin()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun vaiParaLogin() {
+        val direcao = DetalhesProdutoFragmentDirections.actionDetalhesProdutoToLogin()
+        controlador.navigate(direcao)
     }
 
 }
